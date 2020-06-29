043c1de2ec25332ba624e696788b73f64aa5855e
Sasha Babicki <sasha@neighbourhoodholdings.com>
2020-06-29 13:31:00 -0700

    building out add activity card

--- Pipedrive Action Cards.js
+++ Pipedrive Action Cards.js
@@ -1,3 +1,33 @@
+function pipedriveActivityButtonSet(pipedriveId, createdById){
+  var parameters = {
+    "pipedriveId" : pipedriveId,
+    "createdById" : createdById
+  };
+  
+  var activityTypes = {
+    "call" : CardService.Icon.PHONE,
+    "meeting" : CardService.Icon.EVENT_PERFORMER,
+    "task" : CardService.Icon.CLOCK,
+    "deadline" : CardService.Icon.INVITE,
+    "email" : CardService.Icon.EMAIL,
+    "lunch" : CardService.Icon.RESTAURANT_ICON
+  };
+  
+  var buttonSet = CardService.newButtonSet();
+  for(var i in activityTypes){
+    parameters["activity_type"] = i;
+    var button = CardService.newImageButton()
+      .setIcon(activityTypes[i])
+      .setAltText(i)
+      .setOnClickAction(CardService.newAction()
+        .setFunctionName("addPipedriveActivity")
+        .setParameters(parameters)
+      );
+    buttonSet.addButton(button);
+  }
+  return buttonSet;
+}
+
 /*
  * Upload note to Pipedrive contact
  *
@@ -45,7 +75,6 @@ function buildAddPipedriveNotesCard(e){
     .setImageUrl(IMAGES.PIPEDRIVE));
   
   var userDetails = LENDESK_USERS[getUserName()];
-  var currentUserTeam = userDetails.team;
   var fontColour = userDetails.colour;
   
   var addNoteSection = CardService.newCardSection();
@@ -99,28 +128,86 @@ function buildAddPipedriveActivitiesCard(e){
     .setImageUrl(IMAGES.PIPEDRIVE));
   
   var userDetails = LENDESK_USERS[getUserName()];
-  var currentUserTeam = userDetails.team;
   var fontColour = userDetails.colour;
   
   var addActivitySection = CardService.newCardSection();
   
+  var buttonSet = pipedriveActivityButtonSet(pipedriveId, userDetails.id);
+  addActivitySection.addWidget(buttonSet);
+    
+  addActivitySection.addWidget(CardService.newTextInput()
+    .setTitle("Subject")
+    .setFieldName("subject")
+    .setValue("Call with "+title)
+  );
+  
   addActivitySection.addWidget(CardService.newTextInput()
     .setMultiline(true)
-    .setTitle("ADD NEW ACTIVITY")
-    .setFieldName("activity")
+    .setTitle("Note")
+    .setFieldName("note")
   );
-    
-  var parameters = {
-    "pipedriveId" : pipedriveId,
-    "createdById" : userDetails.id
-  };
   
+  addActivitySection.addWidget(CardService.newDateTimePicker()
+    .setTitle("Start Time")
+    .setFieldName("start_date")
+  );
+  addActivitySection.addWidget(CardService.newDateTimePicker()
+    .setTitle("End Time")
+    .setFieldName("end_date")
+  );
+  
+  addActivitySection.addWidget(CardService.newKeyValue()
+    .setTopLabel("Linked To")
+    .setContent(title).setButton(CardService.newTextButton().setText("Change").setOpenLink(CardService.newOpenLink().setUrl("google.ca")))
+  );
+  
+  /*
+  var durationSelection = CardService.newSelectionInput()
+    .setTitle("Duration")
+    .setFieldName("duration")
+    .setType(CardService.SelectionInputType.DROPDOWN);
+  
+  var durationIncrement = 15;
+  var maxDuration = 8*60;
+  
+  function formatTime(time){
+    var formattedTime = "";
+    try{
+      var hours = Math.trunc(time);
+      var minutes = time - hours;
+      minutes = minutes * 60;
+      formattedTime = hours+":"+(minutes == 0 ? "00" : minutes);
+    }
+    catch(e){
+    }
+    return formattedTime;
+  }
+  durationSelection.addItem("", 0, true);
+  for(var i = 1; i <= maxDuration/durationIncrement; i++){
+    durationSelection.addItem(formatTime(i*durationIncrement/60), i*durationIncrement, false);
+  }
+  addActivitySection.addWidget(durationSelection);
+  */
+  
+  var assigneeSelection = CardService.newSelectionInput()
+    .setTitle("Assignee")
+    .setFieldName("assignee")
+    .setType(CardService.SelectionInputType.DROPDOWN);
+  
+  var users = PipedriveAPILibrary.USER_MAP_BY_NAME;
+  for(var i in users){
+    assigneeSelection.addItem(i, users[i], (i == userDetails.name ? true : false));
+  }
+  addActivitySection.addWidget(assigneeSelection);
+  
+  //addActivitySection.addWidget(CardService.newSelectionInput().setType(CardService.SelectionInputType.CHECK_BOX).setFieldName("complete").addItem("Mark as done", 1, true));
+
   addActivitySection.addWidget(CardService.newTextButton()
     .setText("Submit")
     .setOnClickAction(CardService.newAction()
     .setFunctionName("addPipedriveActivity")
-    .setParameters(parameters)));
-  
+    .setParameters({})));
+ 
   card.addSection(addActivitySection);
   
   var displayActivitySection = pipedriveActivityDisplaySection(pipedriveId, [], 50);
